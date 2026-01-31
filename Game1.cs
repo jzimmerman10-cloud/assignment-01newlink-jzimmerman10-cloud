62c4f4680250b271a49d818102cd85e504619da8
jzimmerman10 <jzimmerman10@nait.ca>
2026-01-30 19:27:40 -0700

    Arrow Key movement

--- Game1.cs
+++ Game1.cs
@@ -14,7 +14,10 @@ public class Game1 : Game
     private Texture2D _background;
     private SimpleAnimation _PlayerWalk;
     private Vector2 _PlayerPosition;
-    private Vector2 _MovementInput;
+    private Vector2 _PlayerInput;
+
+    private SpriteFont _Font;
+    private string _message = "It is dangerous to go alone";
 
     public Game1()
     {
@@ -38,13 +41,53 @@ public class Game1 : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
+        _Font = Content.Load<SpriteFont>("SansFont");
 
         // TODO: use this.Content to load your game content here
         _background = Content.Load<Texture2D>("Zelda1Screen");
+
+        _PlayerWalk = new SimpleAnimation(
+            Content.Load<Texture2D>("Zelda1SpriteSheet"),
+            frameWidth: 160,
+            frameHeight: 160,
+            frameCount: 4,
+            framesPerSecond: 60
+        );
     }
 
     protected override void Update(GameTime gameTime)
     {
+        KeyboardState kbCurrentState = Keyboard.GetState();
+        _message = "";
+    _PlayerInput = Vector2.Zero;
+        #region arrow keys
+
+        if(kbCurrentState.IsKeyDown(Keys.Down))
+        {
+            _PlayerInput += new Vector2(0,1);
+            _message += "Down";
+        }
+        if (kbCurrentState.IsKeyDown(Keys.Up))
+        {
+            _PlayerInput += new Vector2(0,-1);
+
+            _message += "Up ";
+        }
+        if (kbCurrentState.IsKeyDown(Keys.Left))
+        {
+            _PlayerInput += new Vector2(-1,0);
+
+            _message += "Left ";
+        }
+        if (kbCurrentState.IsKeyDown(Keys.Right))
+        {
+            _PlayerInput += new Vector2(1,0);
+
+            _message += "Right ";
+        }
+        #endregion
+
+_PlayerPosition += _PlayerInput * 10;
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
@@ -53,6 +96,7 @@ public class Game1 : Game
         base.Update(gameTime);
     }
 
+Vector2 PlayerLocation = new Vector2(300, 140);
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
@@ -62,6 +106,8 @@ public class Game1 : Game
 
         Rectangle BackgroundRect = new Rectangle(0,0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
         _spriteBatch.Draw(_background, BackgroundRect, Color.White);
+        _spriteBatch.DrawString(_Font, _message, Vector2.Zero, Color.Red);
+        _PlayerWalk.Draw(_spriteBatch, _PlayerPosition, SpriteEffects.None);
 
         _spriteBatch.End();
 
